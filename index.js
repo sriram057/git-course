3c1cf4ad1ab21073a0727ee3f888ae38efe05e5f
sriram057 <sriramsingarapu2@gmail.com>
2024-03-02 18:17:41 +0530

    promises vs callbacks

--- index.js
+++ index.js
@@ -1,26 +1,45 @@
-// verable:
 
-// Create a function "bookSearchPromise" that returns a promise which after 3 seconds either resolves with message "Book found" or gets rejected with message "Book not found".
-// Make use of Math.random() to mimic both the resolve and reject case.
-// Handle the fulfillment of the promise with then block and print the resolve message to the console.
-// Handle the rejection of the promise with catch block and print the reject message to the console.
 
-function bookSearchPromise(){
-  return new Promise((resolve,reject) => {
+function buyBike(){
+  return new Promise(function(resolve,reject){
     setTimeout(function(){
-      const bookSearch = Math.random() > 0.5;
-      if(bookSearch){
-        resolve('Book found')
-      }else {
-        reject('Book not found')
-      }
-    },3000)
+      resolve("Bought Royal Enfield Himalayan")
+    },2000)
   })
 }
-bookSearchPromise()
-  .then((result) => {
+
+function planTrip(){
+  return new Promise(function(resolve,reject){
+    setTimeout(function(){
+      resolve("Trip to Ladakh planned")
+    },1000)
+  })
+}
+
+function reachLadakh(){
+  return new Promise(function(resolve,reject){
+    setTimeout(function(){
+      resolve("Reached Ladakh")
+    },1000)
+  })
+}
+
+function visitPangongLake(){
+  setTimeout(function(){
+    console.log("Visited Pangong Lake")
+  },500)
+}
+
+buyBike().then(function(result){
+  console.log(result);
+  
+  planTrip().then(function(result){
     console.log(result);
+    
+    reachLadakh().then(function(result){
+      console.log(result);
+      visitPangongLake();
+    })
   })
-  .catch((error) => {
-    console.log(error)
-  })
+  
+})
