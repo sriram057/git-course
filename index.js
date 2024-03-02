22d093dd40fa1cce8c632919b8556ef3cee50b2c
sriram057 <sriramsingarapu2@gmail.com>
2024-03-02 15:32:50 +0530

    books found

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
-function writeLetter(callback){
-  setTimeout(function (){
-      console.log("Wrote Letter")
-      callback();
-  },2000)
-}
+// verable:
 
-function postLetter(){
-  setTimeout(function(){
-      console.log("Letter Posted")
-  },1000)
-}
+// Create a function "bookSearchPromise" that returns a promise which after 3 seconds either resolves with message "Book found" or gets rejected with message "Book not found".
+// Make use of Math.random() to mimic both the resolve and reject case.
+// Handle the fulfillment of the promise with then block and print the resolve message to the console.
+// Handle the rejection of the promise with catch block and print the reject message to the console.
 
-writeLetter(postLetter);
+function bookSearchPromise(){
+  return new Promise((resolve,reject) => {
+    setTimeout(function(){
+      const bookSearch = Math.random() > 0.5;
+      if(bookSearch){
+        resolve('Book found')
+      }else {
+        reject('Book not found')
+      }
+    },3000)
+  })
+}
+bookSearchPromise()
+  .then((result) => {
+    console.log(result);
+  })
+  .catch((error) => {
+    console.log(error)
+  })
