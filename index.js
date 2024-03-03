259dea2c323e668b08c91587c41bba627339c662
sriram057 <sriramsingarapu2@gmail.com>
2024-03-03 22:43:02 +0530

    appointform crud

--- index.js
+++ index.js
@@ -13,6 +13,32 @@ function handleFormSubmit(event) {
     .then((response) => displayUserOnScreen(response.data))
     .catch((error) => console.log(error));
 
+    axios
+    .get(
+      "https://crudcrud.com/api/944ce205c1ed4a8d8db3a64234f41e25/appointmentData",
+      userDetails
+    )
+    .then((response) => {
+      console.log(response)
+      for(var i=0;i<response.data.length;i++){
+        displayUserOnScreen(response.data[i])
+      }
+    })
+    .catch((error) => console.log(error));
+
+    axios
+    .delete(
+      "https://crudcrud.com/api/944ce205c1ed4a8d8db3a64234f41e25/appointmentData/65e4aa8a1d380403e8714275",
+      userDetails)
+
+      axios.put(
+        "https://crudcrud.com/api/944ce205c1ed4a8d8db3a64234f41e25/appointmentData/65e4aa351d380403e8714272",
+        userDetails  // This should be the data you want to update
+      )
+        .then((response) => displayUserOnScreen(response.data))
+        .catch((error) => console.log(error));
+      
+
   // Clearing the input fields
   document.getElementById("username").value = "";
   document.getElementById("email").value = "";
@@ -38,17 +64,17 @@ function displayUserOnScreen(userDetails) {
   const userList = document.querySelector("ul");
   userList.appendChild(userItem);
 
-  deleteBtn.addEventListener("click", function (event) {
-    userList.removeChild(event.target.parentElement);
-    localStorage.removeItem(userDetails.email);
-  });
+  // deleteBtn.addEventListener("click", function (event) {
+  //   userList.removeChild(event.target.parentElement);
+  //   localStorage.removeItem(userDetails.email);
+  // });
 
-  editBtn.addEventListener("click", function (event) {
-    userList.removeChild(event.target.parentElement);
-    localStorage.removeItem(userDetails.email);
-    document.getElementById("username").value = userDetails.username;
-    document.getElementById("email").value = userDetails.email;
-    document.getElementById("phone").value = userDetails.phone;
-  });
+  // editBtn.addEventListener("click", function (event) {
+  //   userList.removeChild(event.target.parentElement);
+  //   localStorage.removeItem(userDetails.email);
+  //   document.getElementById("username").value = userDetails.username;
+  //   document.getElementById("email").value = userDetails.email;
+  //   document.getElementById("phone").value = userDetails.phone;
+  // });
 }
 
